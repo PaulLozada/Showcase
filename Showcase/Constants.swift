4ca23438300f2fecbeaead2b4cd859e31b9bbc5f
PaulLozada <paul_lozada@icloud.com>
2016-02-21 02:29:56 -0700

    Added user interface elements for login screen

---
+++ Showcase/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Showcase
+//
+//  Created by Paul Lozada on 2016-02-20.
+//  Copyright © 2016 Paul Lozada. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let SHADOW_COLOR : CGFloat = 157.0 / 225.0
