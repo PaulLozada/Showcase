69f018e6539391a0af4168f65a3b8fd1f365e100
PaulLozada <paul_lozada@icloud.com>
2016-02-22 16:50:58 -0700

    Added more info to firebase

--- Showcase/DataService.swift
+++ Showcase/DataService.swift
@@ -9,10 +9,14 @@
 import Foundation
 import Firebase
 
+let URL_BASE = "https://appshowcase1.firebaseio.com/"
+
 class DataService {
     static let ds = DataService( )
     
-    private var _REF_Base = Firebase(url: "https://appshowcase1.firebaseio.com")
+    private var _REF_Base = Firebase(url: "\(URL_BASE)")
+    private var _REF_POSTS = Firebase(url: "\(URL_BASE)/posts")
+    private var _REF_USERS = Firebase(url: "\(URL_BASE)/users")
     
     var REF_Base : Firebase {
         return _REF_Base
