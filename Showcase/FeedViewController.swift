9130e89af7c9dd09311a22598b6cf873a7af13b5
PaulLozada <paul_lozada@icloud.com>
2016-02-24 12:24:56 -0700

    Added support for imageshack

--- Showcase/FeedViewController.swift
+++ Showcase/FeedViewController.swift
@@ -8,12 +8,77 @@
 
 import UIKit
 import Firebase
+import Alamofire
 
-class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataSource, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    
+    var posts = [ Post ]( )
+    var imagePicker = UIImagePickerController( )
+    static var imageCache = NSCache( )
+    
+    
     
     @IBOutlet weak var tableView : UITableView!
-    var posts = [Post]( )
-    static var imageCache = NSCache( )
+    @IBOutlet weak var postField: UITextField!
+    @IBOutlet weak var imageSelectorImage: UIImageView!
+    
+    
+    @IBAction func makePost(sender: UIButton) {
+        
+        if let txt = postField.text where txt != "" {
+            if let img = imageSelectorImage.image {
+                let urlSting = "https://post.imageshack.us/upload_api.php"
+                let url = NSURL(string: urlSting)!
+                let imageData = UIImageJPEGRepresentation(img, 0.2)!
+                let keyData = "267DGMQTae476eb0db1f46e86a128d3c87ac095f".dataUsingEncoding(NSUTF8StringEncoding)!
+                let keyJson = "json".dataUsingEncoding(NSUTF8StringEncoding)!
+                
+                Alamofire.upload(.POST, url, multipartFormData: { multipartFormData in
+                    
+                    multipartFormData.appendBodyPart(data: imageData, name: "fileupload", fileName: "image", mimeType: "image/jpg")
+                    multipartFormData.appendBodyPart(data: keyData, name: "key")
+                    multipartFormData.appendBodyPart(data: keyJson, name: "format")
+                    
+                    })
+                        { encodingResult in
+                    
+                    switch encodingResult {
+                    case .Success(let upload , _ , _ ) :
+                        upload.responseJSON(completionHandler: { response  in
+                            if let info = response.result.value as? [String:AnyObject] {
+                                if let links = info["links"] as? [String:AnyObject] {
+                                    if let imgLink = links["image_link"] as? String {
+                                        
+                                    }
+                                }
+                            }
+                        })
+                        
+                    case .Failure(let error) :
+                        print(error)
+                    }
+                    
+                }
+            }
+            
+        }
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        imageSelectorImage.image = image
+    
+        
+    }
+    
+    @IBAction func passed(sender: UITapGestureRecognizer) {
+        
+          presentViewController(imagePicker, animated: true, completion: nil)
+        
+        
+    }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
@@ -47,6 +112,8 @@ class FeedViewController: UIViewController, UITableViewDelegate,UITableViewDataS
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
 
         DataService.ds.REF_POSTS.observeEventType(.Value, withBlock: { snapshot in
             print(snapshot.value)
